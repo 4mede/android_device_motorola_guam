fb7f44dd558ff11ada5499590ad822a8126a6337
Michael Bestas <mkbestas@lineageos.org>
2025-04-22 18:00:46 +0300

    guam: overlay: Adjust statusbar dimensions
    Change-Id: I3747e0a1a83c28dcecdd8db8a92791942558c2a9

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_guam.mk
+    $(LOCAL_DIR)/arrow_guam.mk
--- arrow_guam.mk
+++ arrow_guam.mk
@@ -12,10 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/guam/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
+
+TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_guam
+PRODUCT_NAME := arrow_guam
 PRODUCT_DEVICE := guam
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto e7 plus
